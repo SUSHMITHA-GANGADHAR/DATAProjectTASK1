bef5e1b04a9031d3084379d273584c0f73bac382
SUSHMITHA-GANGADHAR <sushmithagsushmithag7@gmail.com>
2024-09-06 23:46:38 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,3 +1,8 @@
+'''Create a bar chart or histogram to
+visualize the distribution of a categorical or continuous variable,
+such as the distribution of ages or genders in a population.'''
+
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
